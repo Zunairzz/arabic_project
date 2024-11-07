ac3dfb93f08add9623b18e6e35674dc96747d552
zaman <zamantariq247@gmail.com>
2024-11-07 20:59:49 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Footer from '../src/component/Footer';
-import RegistrationForm from '../src/pages/RegistrationForm'
+import RegistrationForm from '../src/pages/RegistrationForm';
+import RegistrationForm1 from '../src/pages/RegistrationForm1'
+import RegistrationForm2 from '../src/pages/RegistrationForm2'
+import Calendar from '../src/pages/Calendar'
+import ConferencePage from '../src/pages/ConferencePage'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
@@ -10,6 +14,10 @@ function App() {
         <Routes>
           < Route path="/" element={<Footer/>}/>
             < Route path="/reg" element={<RegistrationForm/>}/>
+            < Route path="/reg1" element={<RegistrationForm1/>}/>
+            < Route path="/reg2" element={<RegistrationForm2/>}/>
+            <Route path="/cal" element={<Calendar/>}/>
+            <Route path="/con" element={<ConferencePage/>}/>
         </Routes>
       </BrowserRouter>
   );
--- src/pages/RegistrationForm.js
+++ src/pages/RegistrationForm.js
@@ -4,6 +4,9 @@ import {Base} from "../component/Base";
 
 const RegistrationForm = () => {
     const style = {
+        para:{
+            margin:'0'
+        },
         dot: {
             backgroundColor: '#545454',
             height: '3px',
@@ -14,7 +17,8 @@ const RegistrationForm = () => {
         textField: {
             backgroundColor: 'rgb(237, 237, 237)',
             borderRadius: '20px',
-            color: '#ffffff'
+            color: '#ffffff',
+            textAlign:'right'
         }
     }
     return (
@@ -22,52 +26,66 @@ const RegistrationForm = () => {
            <Container className="mt-5">
                <Row>
                    <Col className="text-center">
-                       <h2 style={{color: '#001489', textAlign: 'right', fontWeight: 'bold'}}>فئات التذاكر</h2>
+                       <h2 style={{color: '#0b1a8a', textAlign: 'right', fontWeight: 'bold'}}>فئات التذاكر</h2>
                        <h3 style={{color: '#009688', textAlign: 'right', fontWeight: 'bold'}}>مزايا فئة VIP:</h3>
                    </Col>
                </Row>
-               <Row style={{textAlign: "right"}}>
+               <Row className= 'mt-2' style={{textAlign: "right"}}>
                    <Col md="6">
-                       <Card className=" border-0" style={{textAlign: 'right', minHeight: '10px'}}>
-                           <CardBody>
-                               <ul className="list-unstyled">
-                                   <li>باجات مخصصة.</li>
-                                   <li>جناح خاص في فندق الفيرمونت.</li>
-                                   <li>المقاعد الأمامية لورش العمل والجلسات الحوارية.</li>
-                                   <li>دعوة لحضور العشاء الختامي مع أصحاب المعالي والضيوف.</li>
-                               </ul>
-                           </CardBody>
-                       </Card>
+                       <div>
+                           <div className="d-inline-flex">
+                               <p style={style.para}>باجات مخصصة</p>
+                               <div style={style.dot}/>
+                           </div>
+                       </div>
+                       <div>
+                           <div className="d-inline-flex">
+                               <p style={style.para}>جناح خاص في فندق الفيرمونت</p>
+                               <div style={style.dot}/>
+                           </div>
+                       </div>
+                       <div>
+                           <div className="d-inline-flex">
+                               <p style={style.para}>المقاعد الأمامية لورش العمل والجلسات الحوارية</p>
+                               <div style={style.dot}/>
+                           </div>
+                       </div>
+                       <div>
+                           <div className="d-inline-flex">
+                               <p style={style.para}>دعوة لحضور العشاء الختامي مع أصحاب المعالي والضيوف</p>
+                               <div style={style.dot}/>
+                           </div>
+                       </div>
                    </Col>
                    <Col md="6">
                        <div>
                            <div className="d-inline-flex">
-                               <p>حضور حفل الافتتاح في مقاعد أمامية مخصصة</p>
+                               <p style={style.para}>حضور حفل الافتتاح في مقاعد أمامية مخصصة</p>
                                <div style={style.dot}/>
                            </div>
                        </div>
                        <div>
                            <div className="d-inline-flex">
-                               <p>خدمة صف السيارات</p>
+                               <p style={style.para}>خدمة صف السيارات</p>
                                <div style={style.dot}/>
                            </div>
                        </div>
                        <div>
                            <div className="d-inline-flex">
-                               <p>استقبال من خلال بوابة مخصصة</p>
+                               <p style={style.para}>استقبال من خلال بوابة مخصصة</p>
                                <div style={style.dot}/>
                            </div>
                        </div>
                        <div>
                            <div className="d-inline-flex">
-                               <p>ضيافة فاخرة</p>
+                               <p style={style.para}>ضيافة فاخرة</p>
                                <div style={style.dot}/>
                            </div>
                        </div>
                    </Col>
                </Row>
                <Row>
-                   <Col className="text-center">
+                   <Col className="text-center m-2">
                        <h4 className="" style={{color: '#333', textAlign: 'right', fontWeight: 'bold'}}>بيانات التسجيل
                            الرئيسية</h4>
                    </Col>
