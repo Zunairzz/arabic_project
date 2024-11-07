ea2df3a486d69816e03d9d30aa47213ce9643183
zaman <zamantariq247@gmail.com>
2024-11-07 23:12:26 +0500

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from '../src/component/Footer';
 import RegistrationForm from '../src/pages/RegistrationForm';
 import RegistrationForm1 from '../src/pages/RegistrationForm1'
 import RegistrationForm2 from '../src/pages/RegistrationForm2'
+import BoxesDetails from './pages/BoxesDetails'
 import Calendar from '../src/pages/Calendar'
 import ConferencePage from '../src/pages/ConferencePage'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
@@ -16,6 +17,7 @@ function App() {
             < Route path="/reg" element={<RegistrationForm/>}/>
             < Route path="/reg1" element={<RegistrationForm1/>}/>
             < Route path="/reg2" element={<RegistrationForm2/>}/>
+            < Route path="/box" element={<BoxesDetails/>}/>
             <Route path="/cal" element={<Calendar/>}/>
             <Route path="/con" element={<ConferencePage/>}/>
         </Routes>
